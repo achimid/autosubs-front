5a1323bd38bd4b0d3f5638b10910b2d09838e409
Lourran Zorzi <lourran-zorzi@lourran.zorzi>
2020-05-18 11:21:40 -0300

    extraçao do projeto front

---
+++ api/index.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+
+const maxAge = process.env.NODE_ENV == 'production' ? 7*86400000 : 0
+
+const cors = require('cors')
+const express = require('express')
+const compression = require('compression')
+const app = express()
+
+app.use(cors())
+app.use(compression())
+app.use(express.json())
+app.disable('x-powered-by')
+
+app.use(express.static('public', { maxAge, extensions:['html'] }))
+app.use('/api/v1/healthcheck', require('./healthcheck/healthcheck'))
+
+app.listen(process.env.PORT)
